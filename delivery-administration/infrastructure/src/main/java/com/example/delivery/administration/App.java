09a289ead6d5d92f200d2680994063f3ff6ff802
Hurado <piaszczynskiarkadiusz@gmail.com>
2023-04-23 15:45:06 +0200

    Added structure with POM

---
+++ delivery-administration/infrastructure/src/main/java/com/example/delivery/administration/App.java
@@ -0,0 +1,13 @@
+package com.example.delivery.administration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
